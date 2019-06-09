f2a5ae0a4f5198bec4f6279c08080d426ee91cbc
Yatender <yatender@getvokal.com>
2019-06-09 22:22:17 +0530

    adding author books doc

--- lib/bookie/author/author_books.ex
+++ lib/bookie/author/author_books.ex
@@ -1,4 +1,10 @@
 defmodule Bookie.AuthorBook do
+  @moduledoc """
+  Module to map author and books
+  to map author with books preloaded structs with associations are required so that
+  two way binding can be done otherwise it'll not work as expected becasue associasion
+  can not be updated via that process.
+  """
   use Bookie, :model
 
   use Ecto.Schema
@@ -11,6 +17,17 @@ defmodule Bookie.AuthorBook do
     belongs_to(:books, Book, type: :binary_id, foreign_key: :book_id)
   end
 
+  @doc """
+  This module is getting used to map the user with the method
+  Here we are takeing the all previous books authored by author
+  and here in author and book module we are expecting the preloaded data
+  of all associations with books and author and then we are mapping it with new method.
+  as on update we are deleting all previous associations so have to map new ones with older
+  ones and update the reference.
+
+  same we will do with delete mapping there instead of adding into the list we'll just
+  delete from the list.
+  """
   def map_author_book(author, books) do
     author_books = author.books
 
